d15a826b940732cfa69187fd2d155161bdaca4ca
D-CYL <118185691+D-CYL@users.noreply.github.com>
2025-12-17 10:54:58 +0100

    delete TestConnection files for clean db connection

--- src/main/java/nl/helpingheart/HelpingHeart/repository/TestConnectionRepository.java
+++
@@ -1,7 +0,0 @@
-package nl.helpingheart.HelpingHeart.repository;
-
-import nl.helpingheart.HelpingHeart.model.TestConnection;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestConnectionRepository extends JpaRepository<TestConnection, Long> {
-}
