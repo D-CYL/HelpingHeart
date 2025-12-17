3b6a8961f057deb00617e29727b9a6bbc5a63ec5
D-CYL <118185691+D-CYL@users.noreply.github.com>
2025-12-17 10:43:34 +0100

    test connection for api

---
+++ src/main/java/nl/helpingheart/HelpingHeart/model/TestConnection.java
@@ -0,0 +1,11 @@
+package nl.helpingheart.HelpingHeart.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class TestConnection {
+
+    @Id
+    private Long id;
+}
