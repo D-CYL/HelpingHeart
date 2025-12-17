cd8ecef12fd767544f4a873f0cf93176931b0b99
D-CYL <118185691+D-CYL@users.noreply.github.com>
2025-12-17 10:51:13 +0100

    test connection for api repo

---
+++ src/main/java/nl/helpingheart/HelpingHeart/repository/TestConnectionRepository.java
@@ -0,0 +1,7 @@
+package nl.helpingheart.HelpingHeart.repository;
+
+import nl.helpingheart.HelpingHeart.model.TestConnection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestConnectionRepository extends JpaRepository<TestConnection, Long> {
+}
